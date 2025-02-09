e3709d5b041b2775160f23e6e824ea534d5479ba
zahiakp <121743451+zahiakp@users.noreply.github.com>
2025-02-09 10:27:09 +0530

    founder to ceo

--- app/(user)/donate/[id]/Modal.tsx
+++ app/(user)/donate/[id]/Modal.tsx
@@ -9,7 +9,7 @@ function Modal({visible,setVisible,paymentData}:{visible: any, setVisible: any,p
       const state = paymentData?.data?.data?.state || 'N/A';
       const paymentInstrument = paymentData?.data?.data?.paymentInstrument || { type: 'N/A', accountType: 'N/A' };
   return (
-    <Dialog header="Transaction Details" draggable={false} visible={visible} style={{ width: '40vw'}} className='p-5 rounded-3xl bg-white' onHide={() => {if (!visible) return; setVisible(false); }}>
+    <Dialog header="Transaction Details" draggable={false} visible={visible} className='p-5 w-[90%] md:w-1/2 rounded-3xl bg-white' onHide={() => {if (!visible) return; setVisible(false); }}>
      
          <div
           className="text-left bg-green-50 p-4 rounded-lg"
@@ -20,12 +20,12 @@ function Modal({visible,setVisible,paymentData}:{visible: any, setVisible: any,p
               <span className="font-semibold">Amount:</span>
               <span className="ml-2">₹{(amount / 100).toFixed(2)}</span>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center flex-wrap">
               <HiCheckCircle className="text-green-500 mr-2" />
               <span className="font-semibold">Transaction ID:</span>
               <span className="ml-2">{transactionId}</span>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center flex-wrap">
               <HiCheckCircle className="text-green-500 mr-2" />
               <span className="font-semibold">Payment Method:</span>
               <span className="ml-2 capitalize">
